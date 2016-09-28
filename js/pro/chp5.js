ee7f6b2f128eb31acddb33e9808131b90e578499
sorrowhere <sorrowhere@gmail.com>
2016-09-28 11:36:09 +0800

    chp5

--- js/pro/chp5.js
+++ js/pro/chp5.js
@@ -28,17 +28,63 @@ console.log(colors2[4]);
 
 
 
+console.log("=================转换方法========================");
+/* 转换方法
+ * =================================================*/
+var person1 = {
+    toLocaleString : function(){
+        return "ABC";
+    },
+    toString : function(){
+        return "XYZ";
+    }
+}
+
+var person2 = {
+    toLocaleString : function(){
+        return "DEF";
+    },
+    toString : function(){
+        return "HIJ";
+    }
+}
+
+var people = [person1, person2];
+console.log(people);
+console.log(people.toString());
+console.log(people.toLocaleString());
 
 
 
+console.log("=================栈方法========================");
+/* 栈方法
+ * =================================================*/
+var color = [];
+var count = color.push("red","blue");
+console.log(count);
+
+count =  color.push("yellow");
+console.log(count);
+
+var item = color.pop();
+console.log(item);
+console.log(color.length);
 
 
 
+console.log("=================队列方法========================");
+/* 队列方法
+ * =================================================*/
+var group = new Array();
+var count1 = group.push("1","2");
+console.log(count1);
 
+count1 = group.push("3");
+console.log(count1);
 
-
-
-
+var item1 = group.shift();
+console.log(item1);
+console.log(group.length);
 
 
 
